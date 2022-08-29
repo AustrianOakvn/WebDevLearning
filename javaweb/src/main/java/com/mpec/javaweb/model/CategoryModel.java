3de7fbff0ad2d6289ad0e7fdaa3ac8be467efe4d
AustrianOakvn <austrian.oak.vn@gmail.com>
2022-08-29 11:05:12 +0700

    add java classes

--- javaweb/src/main/java/com/mpec/javaweb/model/CategoryModel.java
+++ javaweb/src/main/java/com/mpec/javaweb/model/CategoryModel.java
@@ -1,5 +1,26 @@
 package com.mpec.javaweb.model;
 
 public class CategoryModel {
+
+    private String name;
+    private String code;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
+
     
 }
